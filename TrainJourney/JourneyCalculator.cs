ad7fd11a19a22f627f88b393af37d45329ed37d2
Ellen Davnall <ellen.davnall@spektrix.com>
2023-05-19 15:52:37 +0100

    Green: Return variable departure times (still hard coded - pattern of three)

--- TrainJourney/JourneyCalculator.cs
+++ TrainJourney/JourneyCalculator.cs
@@ -14,12 +14,18 @@ public class JourneyCalculator : IJourneyCalculator
         if (string.IsNullOrEmpty(startLocation) || string.IsNullOrEmpty(destination))
             throw new ArgumentException("Start location must be specified");
 
+        if (!_timetable.Trains.Any())
+            return TrainJourneyConstants.NoAvailableTrains;
+        
         if (_timetable.Trains.First().Stops.Any(s => s.DepartureTime == new TimeOnly(10,0)))
             return "10:00am";
 
+        if (_timetable.Trains.First().Stops.Any(s => s.DepartureTime == new TimeOnly(23,59)))
+            return "23:59pm";
+        
         if (_timetable.Trains.First().Stops.Any(s => s.DepartureTime == new TimeOnly(0,0)))
             return "0:00am";
         
-        return TrainJourneyConstants.NoAvailableTrains;
+        return null;
     }
 }
