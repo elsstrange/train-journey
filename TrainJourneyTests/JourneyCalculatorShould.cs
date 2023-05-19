ade7d331734d3b23dc291ae89d3ab7da9ba8ee42
Ellen Davnall <ellen.davnall@spektrix.com>
2023-05-19 14:57:12 +0100

    Refactor: Extract test constants

--- TrainJourneyTests/JourneyCalculatorShould.cs
+++ TrainJourneyTests/JourneyCalculatorShould.cs
@@ -4,6 +4,10 @@ namespace TrainJourneyTests;
 
 public class JourneyCalculatorShould
 {
+    private const string AStartLocation = "StartLocation";
+    private const string ADestination = "Destination";
+    private const string Nowhere = "";
+
     [SetUp]
     public void Setup()
     {
@@ -12,19 +16,19 @@ public class JourneyCalculatorShould
     [Test]
     public void Return_no_available_trains_when_there_is_no_timetable()
     {
-        var result = new JourneyCalculator().GetNextTrainTime("StartLocation", "Destination");
+        var result = new JourneyCalculator().GetNextTrainTime(AStartLocation, ADestination);
         Assert.That(result, Is.EqualTo(TrainJourneyConstants.NoAvailableTrains));
     }
 
     [Test]
     public void Throw_exception_when_no_start_location_provided()
     {
-        Assert.Throws<ArgumentException>(() => new JourneyCalculator().GetNextTrainTime("", "Destination"));
+        Assert.Throws<ArgumentException>(() => new JourneyCalculator().GetNextTrainTime(Nowhere, ADestination));
     }
 
     [Test]
     public void Throw_exception_when_no_destination_provided()
     {
-        Assert.Throws<ArgumentException>(() => new JourneyCalculator().GetNextTrainTime("StartLocation", ""));
+        Assert.Throws<ArgumentException>(() => new JourneyCalculator().GetNextTrainTime(AStartLocation, Nowhere));
     }
 }
