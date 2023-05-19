dfcbbe3bf8257814891d520a04d6df8ccb44c9b5
Ellen Davnall <ellen.davnall@spektrix.com>
2023-05-19 14:53:09 +0100

    Green: Throw when no start location given

--- TrainJourney/JourneyCalculator.cs
+++ TrainJourney/JourneyCalculator.cs
@@ -4,6 +4,9 @@ public class JourneyCalculator : IJourneyCalculator
 {
     public string GetNextTrainTime(string startLocation, string destination)
     {
+        if (string.IsNullOrEmpty(startLocation))
+            throw new ArgumentException("Start location must be specified");
+        
         return TrainJourneyConstants.NoAvailableTrains;
     }
 }
